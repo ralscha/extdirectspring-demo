97f79619e5fbb03ea60aaee1d459870633697c3c
Ralph Schaer <ralphschaer@gmail.com>
2012-08-01 02:45:19 +0200

    Format source

--- src/main/webapp/extjs41/carstore/app.js
+++ src/main/webapp/extjs41/carstore/app.js
@@ -1,17 +1,17 @@
 Ext.Loader.setConfig({
-    enabled: true,
-    paths: {
-        'carstore': 'app'
-    }
+	enabled: true,
+	paths: {
+		'carstore': 'app'
+	}
 });
 
-Ext.onReady( function () {
+Ext.onReady(function() {
 	Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
-	
-    Deft.Injector.configure({
-        carChart: 'carstore.store.CarChart',
-        carData: 'carstore.store.CarData'
-    });
-    
-    Ext.create('carstore.view.CarListings');
+
+	Deft.Injector.configure({
+		carChart: 'carstore.store.CarChart',
+		carData: 'carstore.store.CarData'
+	});
+
+	Ext.create('carstore.view.CarListings');
 });
