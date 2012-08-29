63d9526fbc8b8104921e3a790f0f38554e10a7cc
Ralph Schaer <ralphschaer@gmail.com>
2012-08-29 13:12:33 +0200

    Source Code format

--- src/main/java/ch/ralscha/extdirectspring/demo/filterbar/IdText.java
+++ src/main/java/ch/ralscha/extdirectspring/demo/filterbar/IdText.java
@@ -29,18 +29,23 @@ public class IdText implements Comparable<IdText> {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		IdText other = (IdText) obj;
 		if (id == null) {
-			if (other.id != null)
+			if (other.id != null) {
 				return false;
-		} else if (!id.equals(other.id))
+			}
+		} else if (!id.equals(other.id)) {
 			return false;
+		}
 		return true;
 	}
 
