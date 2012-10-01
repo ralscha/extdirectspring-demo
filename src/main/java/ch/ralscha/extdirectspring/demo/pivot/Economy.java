8952f7a7b1b0c6cc1c7d91849cb648c3bb900032
Ralph Schaer <ralphschaer@gmail.com>
2012-10-01 14:58:08 +0200

    Add license header

--- src/main/java/ch/ralscha/extdirectspring/demo/pivot/Economy.java
+++ src/main/java/ch/ralscha/extdirectspring/demo/pivot/Economy.java
@@ -1,18 +1,33 @@
+/**
+ * Copyright 2010-2012 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.ralscha.extdirectspring.demo.pivot;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Economy {
 
-	private String economy;
+	private final String economy;
 
-	private String region;
+	private final String region;
 
-	private int year;
+	private final int year;
 
-	private int procedures;
+	private final int procedures;
 
-	private int time;
+	private final int time;
 
 	public Economy(String economy, String region, int year, int procedures, int time) {
 		this.economy = economy;
--- src/main/java/ch/ralscha/extdirectspring/demo/pivot/PivotService.java
+++ src/main/java/ch/ralscha/extdirectspring/demo/pivot/PivotService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2010-2012 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.ralscha.extdirectspring.demo.pivot;
 
 import java.io.BufferedReader;
@@ -55,7 +70,7 @@ public class PivotService {
 		ImmutableList.Builder<Economy> builder = ImmutableList.builder();
 		for (String economy : countriesMap.keySet()) {
 
-			for (int i = 0; i < ThreadLocalRandom.current().nextInt(1,3); i++) {
+			for (int i = 0; i < ThreadLocalRandom.current().nextInt(1, 3); i++) {
 				for (int year = 2004; year <= currentYear; year++) {
 					int procedure = ThreadLocalRandom.current().nextInt(21);
 					int time = ThreadLocalRandom.current().nextInt(200);
