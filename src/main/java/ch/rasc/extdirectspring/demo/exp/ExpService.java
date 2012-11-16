c65c5c5906f4e8b20de8ec4c2cddcec51273ebf5
Ralph Schaer <ralphschaer@gmail.com>
2012-11-16 10:50:08 +0100

    Some more experiments with associations

--- src/main/java/ch/rasc/extdirectspring/demo/exp/ExpService.java
+++ src/main/java/ch/rasc/extdirectspring/demo/exp/ExpService.java
@@ -1,5 +1,21 @@
+/**
+ * Copyright 2010-2012 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.rasc.extdirectspring.demo.exp;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.joda.time.LocalDate;
@@ -24,6 +40,11 @@ public class ExpService {
 		b.setNumberOfPages(600);
 		b.setRead(false);
 
+		return Collections.singletonList(b);
+	}
+
+	@ExtDirectMethod(value = ExtDirectMethodType.STORE_READ, group = "exp")
+	public List<Author> readAuthors() {
 		Author author1 = new Author();
 		author1.setFirstName("Jesus");
 		author1.setLastName("Garcia");
@@ -39,9 +60,7 @@ public class ExpService {
 		author3.setLastName("Grisogono");
 		author3.setTitle("Mr.");
 
-		b.setAuthors(ImmutableList.of(author1, author2, author3));
-
-		return ImmutableList.of(b);
+		return ImmutableList.of(author1, author2, author3);
 	}
 
 }
