5e0f85e91c6d88475eeb28e244d59da1366d4da4
Ralph Schaer <ralphschaer@gmail.com>
2012-12-01 09:47:19 +0100

    Use the new SSE handling from eds 1.2.4. This now also works in IE

--- src/main/java/ch/rasc/extdirectspring/demo/Poll.java
+++ src/main/java/ch/rasc/extdirectspring/demo/Poll.java
@@ -17,6 +17,7 @@ package ch.rasc.extdirectspring.demo;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ch.ralscha.extdirectspring.annotation.ExtDirectMethod;
 import ch.ralscha.extdirectspring.annotation.ExtDirectMethodType;
 import ch.ralscha.extdirectspring.bean.SSEvent;
+import ch.ralscha.extdirectspring.controller.SSEWriter;
 
 @Service
 public class Poll {
@@ -44,12 +46,30 @@ public class Poll {
 	}
 
 	@ExtDirectMethod(value = ExtDirectMethodType.SSE, group = "example")
-	public SSEvent sse() {
+	public SSEvent sse(SSEWriter sseWriter) {
+
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd 'at' hh:mm:ss");
+
+		for (int i = 0; i < 20; i++) {
+
+			try {
+				sseWriter.write("Successfully polled with EventSource at: " + formatter.format(new Date()));
+			} catch (Exception e) {
+				return null;
+			}
+
+			try {
+				TimeUnit.SECONDS.sleep(3);
+			} catch (InterruptedException e) {
+				return null;
+			}
+		}
 
 		SSEvent event = new SSEvent();
 		event.setRetry(3000);
-		event.setData("Successfully polled with EventSource at: " + formatter.format(new Date()));
+		event.setData("LAST: Successfully polled with EventSource at: " + formatter.format(new Date()));
 		return event;
+
 	}
+
 }
