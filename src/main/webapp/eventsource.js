07344c0e4c2639d22139f5567c35c84cd2a50ca4
Ralph Schaer <ralphschaer@gmail.com>
2013-09-27 08:46:51 +0200

    Update libraries

--- src/main/webapp/eventsource.js
+++ src/main/webapp/eventsource.js
@@ -5,7 +5,7 @@
  */
 
 /*jslint indent: 2, vars: true, plusplus: true */
-/*global setTimeout, clearTimeout, navigator */
+/*global setTimeout, clearTimeout */
 
 (function (global) {
   "use strict";
@@ -38,6 +38,7 @@
 
   EventTarget.prototype = {
     dispatchEvent: function (event) {
+      event.target = this;
       var type = String(event.type);
       var listeners = this.listeners;
       var typeListeners = listeners.get(type);
@@ -46,8 +47,9 @@
       }
       var length = typeListeners.length;
       var i = -1;
+      var listener = null;
       while (++i < length) {
-        var listener = typeListeners[i];
+        listener = typeListeners[i];
         try {
           listener.call(this, event);
         } catch (e) {
@@ -60,7 +62,8 @@
       var listeners = this.listeners;
       var typeListeners = listeners.get(type);
       if (!typeListeners) {
-        listeners.set(type, typeListeners = []);
+        typeListeners = [];
+        listeners.set(type, typeListeners);
       }
       var i = typeListeners.length;
       while (--i >= 0) {
@@ -95,6 +98,7 @@
 
   function Event(type) {
     this.type = type;
+    this.target = null;
   }
 
   function MessageEvent(type, options) {
@@ -107,25 +111,32 @@
 
   var XHR = global.XMLHttpRequest;
   var XDR = global.XDomainRequest;
-  var xhr2 = Boolean(XHR && ((new XHR()).withCredentials !== undefined));
-  var isXHR = xhr2;
-  var Transport = xhr2 ? XHR : XDR;
+  var isCORSSupported = Boolean(XHR && ((new XHR()).withCredentials !== undefined));
+  var isXHR = isCORSSupported;
+  var Transport = isCORSSupported ? XHR : XDR;
   var WAITING = -1;
   var CONNECTING = 0;
   var OPEN = 1;
   var CLOSED = 2;
+  var AFTER_CR = 3;
+  var FIELD_START = 4;
+  var FIELD = 5;
+  var VALUE_START = 6;
+  var VALUE = 7;
   var contentTypeRegExp = /^text\/event\-stream;?(\s*charset\=utf\-8)?$/i;
-  var webkitBefore535 = /AppleWebKit\/5([0-2][0-9]|3[0-4])[^\d]/.test(navigator.userAgent);
+
+  var MINIMUM_DURATION = 1000;
+  var MAXIMUM_DURATION = 18000000;
 
   function getDuration(value, def) {
-    var n = Number(value);
-    return (n < 1 ? 1 : (n > 18000000 ? 18000000 : n)) || def;
+    var n = Number(value) || def;
+    return (n < MINIMUM_DURATION ? MINIMUM_DURATION : (n > MAXIMUM_DURATION ? MAXIMUM_DURATION : n));
   }
 
-  function fire(f, event) {
+  function fire(that, f, event) {
     try {
       if (typeof f === "function") {
-        f(event);
+        f.call(that, event);
       }
     } catch (e) {
       throwError(e);
@@ -135,9 +146,8 @@
   function EventSource(url, options) {
     url = String(url);
 
-    var withCredentials = Boolean(xhr2 && options && options.withCredentials);
+    var withCredentials = Boolean(isCORSSupported && options && options.withCredentials);
     var initialRetry = getDuration(options ? options.retry : NaN, 1000);
-    var retryLimit = getDuration(options ? options.retryLimit : NaN, 300000);
     var heartbeatTimeout = getDuration(options ? options.heartbeatTimeout : NaN, 45000);
     var lastEventId = (options && options.lastEventId && String(options.lastEventId)) || "";
     var that = this;
@@ -145,15 +155,17 @@
     var wasActivity = false;
     var xhr = new Transport();
     var timeout = 0;
+    var timeout0 = 0;
     var charOffset = 0;
     var currentState = WAITING;
     var dataBuffer = [];
     var lastEventIdBuffer = "";
     var eventTypeBuffer = "";
-    var responseBuffer = [];
-    var wasCR = false;
-    var progressTimeout = 0;
-    var wasAct = false;
+    var onTimeout = null;
+
+    var state = FIELD_START;
+    var field = "";
+    var value = "";
 
     options = null;
 
@@ -167,64 +179,81 @@
         clearTimeout(timeout);
         timeout = 0;
       }
+      if (timeout0 !== 0) {
+        clearTimeout(timeout0);
+        timeout0 = 0;
+      }
       that.readyState = CLOSED;
     }
 
     function onProgress(isLoadEnd) {
       var responseText = currentState === OPEN || currentState === CONNECTING ? xhr.responseText || "" : "";
       var event = null;
+      var isWrongStatusCodeOrContentType = false;
 
       if (currentState === CONNECTING) {
-        // invalid state error when xhr.getResponseHeader called after xhr.abort or before readyState === 2 in Chrome 18
-        var contentType = isXHR ? (responseText !== "" ? xhr.getResponseHeader("Content-Type") : "") : xhr.contentType;
-        //var status = isXHR ? (responseText !== "" ? xhr.status : 0) : 200;
-        if (contentType && contentTypeRegExp.test(contentType)) {
+        var status = 0;
+        var statusText = "";
+        var contentType = "";
+        if (isXHR) {
+          try {
+            status = Number(xhr.status || 0);
+            statusText = String(xhr.statusText || "");
+            contentType = String(xhr.getResponseHeader("Content-Type") || "");
+          } catch (error) {
+            // https://bugs.webkit.org/show_bug.cgi?id=29121
+            status = 0;
+            // FF < 14, WebKit
+            // https://bugs.webkit.org/show_bug.cgi?id=29658
+            // https://bugs.webkit.org/show_bug.cgi?id=77854
+          }
+        } else {
+          status = 200;
+          contentType = xhr.contentType;
+        }
+        if (status === 200 && contentTypeRegExp.test(contentType)) {
           currentState = OPEN;
           wasActivity = true;
           retry = initialRetry;
           that.readyState = OPEN;
           event = new Event("open");
           that.dispatchEvent(event);
-          fire(that.onopen, event);
+          fire(that, that.onopen, event);
           if (currentState === CLOSED) {
             return;
+          }
+        } else {
+          if (status !== 0) {
+            var message = "";
+            if (status !== 200) {
+              message = "EventSource's response has a status " + status + " " + statusText.replace(/\s+/g, " ") + " that is not 200. Aborting the connection.";
+            } else {
+              message = "EventSource's response has a Content-Type specifying an unsupported type: " + contentType.replace(/\s+/g, " ") + ". Aborting the connection.";
+            }
+            setTimeout(function () {
+              throw new Error(message);
+            });
+            isWrongStatusCodeOrContentType = true;
           }
         }
       }
 
       if (currentState === OPEN) {
         if (responseText.length > charOffset) {
-          wasAct = true;
           wasActivity = true;
         }
-        var i = 0;
-        var i1 = responseText.indexOf("\r", charOffset);
-        var i2 = responseText.indexOf("\n", charOffset);
-        while (i1 !== -1 || i2 !== -1) {
-          if (i1 === -1 || (i2 !== -1 && i2 < i1)) {
-            i = i2;
-            i2 = responseText.indexOf("\n", i + 1);
+        var i = charOffset - 1;
+        var length = responseText.length;
+        var c = "\n";
+        while (++i < length) {
+          c = responseText[i];
+          if (state === AFTER_CR && c === "\n") {
+            state = FIELD_START;
           } else {
-            i = i1;
-            i1 = responseText.indexOf("\r", i + 1);
-          }
-          var line = responseText.slice(charOffset, i);
-          var oldWasCR = wasCR;
-          wasCR = responseText.slice(i, i + 1) === "\r";
-          charOffset = i + 1;
-          if (!oldWasCR || line.length !== 0 || wasCR) {
-            responseBuffer.push(line);
-            var field = responseBuffer.join("");
-            responseBuffer.length = 0;
-
-            if (field !== "") {
-              var value = "";
-              var j = field.indexOf(":", 0);
-              if (j !== -1) {
-                value = field.slice(j + (field.slice(j + 1, j + 2) === " " ? 2 : 1));
-                field = field.slice(0, j);
-              }
-
+            if (state === AFTER_CR) {
+              state = FIELD_START;
+            }
+            if (c === "\r" || c === "\n") {
               if (field === "data") {
                 dataBuffer.push(value);
               } else if (field === "id") {
@@ -234,11 +263,6 @@
               } else if (field === "retry") {
                 initialRetry = getDuration(value, initialRetry);
                 retry = initialRetry;
-                if (retryLimit < initialRetry) {
-                  retryLimit = initialRetry;
-                }
-              } else if (field === "retryLimit") {//!
-                retryLimit = getDuration(value, retryLimit);
               } else if (field === "heartbeatTimeout") {//!
                 heartbeatTimeout = getDuration(value, heartbeatTimeout);
                 if (timeout !== 0) {
@@ -246,52 +270,67 @@
                   timeout = setTimeout(onTimeout, heartbeatTimeout);
                 }
               }
-
-            } else {
-              // dispatch the event
-              if (dataBuffer.length !== 0) {
-                lastEventId = lastEventIdBuffer;
-                var type = eventTypeBuffer || "message";
-                event = new MessageEvent(type, {
-                  data: dataBuffer.join("\n"),
-                  lastEventId: lastEventIdBuffer
-                });
-                that.dispatchEvent(event);
-                if (type === "message") {
-                  fire(that.onmessage, event);
+              value = "";
+              field = "";
+              if (state === FIELD_START) {
+                if (dataBuffer.length !== 0) {
+                  lastEventId = lastEventIdBuffer;
+                  if (eventTypeBuffer === "") {
+                    eventTypeBuffer = "message";
+                  }
+                  event = new MessageEvent(eventTypeBuffer, {
+                    data: dataBuffer.join("\n"),
+                    lastEventId: lastEventIdBuffer
+                  });
+                  that.dispatchEvent(event);
+                  if (eventTypeBuffer === "message") {
+                    fire(that, that.onmessage, event);
+                  }
+                  if (currentState === CLOSED) {
+                    return;
+                  }
                 }
-                if (currentState === CLOSED) {
-                  return;
-                }
+                dataBuffer.length = 0;
+                eventTypeBuffer = "";
               }
-              // Set the data buffer and the event name buffer to the empty string.
-              dataBuffer.length = 0;
-              eventTypeBuffer = "";
+              state = c === "\r" ? AFTER_CR : FIELD_START;
+            } else {
+              if (state === FIELD_START) {
+                state = FIELD;
+              }
+              if (state === FIELD) {
+                if (c === ":") {
+                  state = VALUE_START;
+                } else {
+                  field += c;
+                }
+              } else if (state === VALUE_START) {
+                if (c !== " ") {
+                  value += c;
+                }
+                state = VALUE;
+              } else if (state === VALUE) {
+                value += c;
+              }
             }
           }
         }
-        if (charOffset !== responseText.length) {
-          responseBuffer.push(responseText.slice(charOffset));
-          charOffset = responseText.length;
-        }
-      }
-
-      // workaround for Opera issue
-      if (wasAct && progressTimeout === 0) {
-        wasAct = false;
-        progressTimeout = setTimeout(p, 80);
+        charOffset = length;
       }
 
       if ((currentState === OPEN || currentState === CONNECTING) &&
-          (isLoadEnd || (charOffset > 1024 * 1024) || (timeout === 0 && !wasActivity))) {
+          (isLoadEnd || isWrongStatusCodeOrContentType || (charOffset > 1024 * 1024) || (timeout === 0 && !wasActivity))) {
         currentState = WAITING;
         xhr.abort();
         if (timeout !== 0) {
           clearTimeout(timeout);
           timeout = 0;
         }
-        if (retry > retryLimit) {
-          retry = retryLimit;
+        if (retry > initialRetry * 16) {
+          retry = initialRetry * 16;
+        }
+        if (retry > MAXIMUM_DURATION) {
+          retry = MAXIMUM_DURATION;
         }
         timeout = setTimeout(onTimeout, retry);
         retry = retry * 2 + 1;
@@ -299,18 +338,13 @@
         that.readyState = CONNECTING;
         event = new Event("error");
         that.dispatchEvent(event);
-        fire(that.onerror, event);
+        fire(that, that.onerror, event);
       } else {
         if (timeout === 0) {
           wasActivity = false;
           timeout = setTimeout(onTimeout, heartbeatTimeout);
         }
       }
-    }
-
-    function p() {
-      progressTimeout = 0;
-      onProgress(false);
     }
 
     function onProgress2() {
@@ -321,34 +355,45 @@
       onProgress(true);
     }
 
-    function onTimeout() {
+    if (isXHR) {
+      // workaround for Opera issue with "progress" events
+      timeout0 = setTimeout(function f() {
+        if (xhr.readyState === 3) {
+          onProgress2();
+        }
+        timeout0 = setTimeout(f, 500);
+      }, 0);
+    }
+
+    onTimeout = function () {
       timeout = 0;
       if (currentState !== WAITING) {
         onProgress(false);
         return;
       }
-      if (navigator.onLine === false) {
-        // "online" event is not supported under Web Workers
-        timeout = setTimeout(onTimeout, 500);
-        return;
-      }
-      // loading indicator in Safari, Chrome < 14
-      if (webkitBefore535 && global.document && (global.document.readyState === "loading" || global.document.readyState === "interactive")) {
-        timeout = setTimeout(onTimeout, 100);
+      // loading indicator in Safari, Chrome < 14, Firefox
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=736723
+      if (isXHR && (xhr.sendAsBinary !== undefined || xhr.onloadend === undefined) && global.document && global.document.readyState && global.document.readyState !== "complete") {
+        timeout = setTimeout(onTimeout, 4);
         return;
       }
       // XDomainRequest#abort removes onprogress, onerror, onload
 
       xhr.onload = xhr.onerror = onLoadEnd;
 
-      // onprogress fires multiple times while readyState === 3
-      // onprogress should be setted before calling "open" for Firefox 3.6
-      if (xhr.mozAnon === undefined) {// Firefox shows loading indicator
-        xhr.onprogress = onProgress2;
-      } else {
-        // Firefox 3.6
+      if (isXHR) {
+        // improper fix to match Firefox behaviour, but it is better than just ignore abort
+        // see https://bugzilla.mozilla.org/show_bug.cgi?id=768596
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=880200
+        // https://code.google.com/p/chromium/issues/detail?id=153570
+        xhr.onabort = onLoadEnd;
+
+        // Firefox 3.5 - 3.6 - ? < 9.0
+        // onprogress is not fired sometimes or delayed
         xhr.onreadystatechange = onProgress2;
       }
+
+      xhr.onprogress = onProgress2;
 
       wasActivity = false;
       timeout = setTimeout(onTimeout, heartbeatTimeout);
@@ -357,18 +402,25 @@
       currentState = CONNECTING;
       dataBuffer.length = 0;
       eventTypeBuffer = "";
-      lastEventIdBuffer = lastEventId;//resets to last successful
-      responseBuffer.length = 0;
-      wasCR = false;
+      lastEventIdBuffer = lastEventId;
+      value = "";
+      field = "";
+      state = FIELD_START;
 
-      xhr.open("GET", url + ((url.indexOf("?", 0) === -1 ? "?" : "&") + "lastEventId=" + encodeURIComponent(lastEventId) + "&r=" + String(Math.random() + 1).slice(2)), true);
-
-      // withCredentials should be setted after "open" for Safari and Chrome (< 19 ?)
-      xhr.withCredentials = withCredentials;
-
-      xhr.responseType = "text";
+      var s = url.slice(0, 5);
+      if (s !== "data:" && s !== "blob:") {
+        s = url + ((url.indexOf("?", 0) === -1 ? "?" : "&") + "lastEventId=" + encodeURIComponent(lastEventId) + "&r=" + String(Math.random() + 1).slice(2));
+      } else {
+        s = url;
+      }
+      xhr.open("GET", s, true);
 
       if (isXHR) {
+        // withCredentials should be set after "open" for Safari and Chrome (< 19 ?)
+        xhr.withCredentials = withCredentials;
+
+        xhr.responseType = "text";
+
         // Request header field Cache-Control is not allowed by Access-Control-Allow-Headers.
         // "Cache-control: no-cache" are not honored in Chrome and Firefox
         // https://bugzilla.mozilla.org/show_bug.cgi?id=428916
@@ -379,7 +431,7 @@
       }
 
       xhr.send(null);
-    }
+    };
 
     EventTarget.call(this);
     this.close = close;
@@ -405,6 +457,13 @@
   F.call(EventSource);
 
   if (Transport) {
+    // Why replace a native EventSource ?
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=444328
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=831392
+    // https://code.google.com/p/chromium/issues/detail?id=260144
+    // https://code.google.com/p/chromium/issues/detail?id=225654
+    // ...
+    global.NativeEventSource = global.EventSource;
     global.EventSource = EventSource;
   }
 
