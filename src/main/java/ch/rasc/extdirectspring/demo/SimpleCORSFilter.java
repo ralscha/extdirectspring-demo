9967108b1ee31acd59a209e3a030dffcda2b8c5e
Ralph Schaer <ralphschaer@gmail.com>
2014-08-02 18:50:25 +0200

    Add a simple CORS filter, so that we can make calls from sencha fiddle

---
+++ src/main/java/ch/rasc/extdirectspring/demo/SimpleCORSFilter.java
@@ -0,0 +1,40 @@
+package ch.rasc.extdirectspring.demo;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "https://fiddle.sencha.com");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept");
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) {
+		// nothing here
+	}
+
+	@Override
+	public void destroy() {
+		// nothing here
+	}
+
+}
