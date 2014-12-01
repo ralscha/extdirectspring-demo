b35526fe28329e1223851e3173a39f2f71016b32
Ralph Schaer <ralphschaer@gmail.com>
2014-12-01 07:10:49 +0100

    Upgrade to latest extjs 5.1 nightly

--- src/main/resources/static/extjs42/sch/bbc.js
+++ src/main/resources/static/extjs42/sch/bbc.js
@@ -98,7 +98,7 @@ Ext.onReady(function() {
 			align: 'center',
 			width: 150,
 			dataIndex: 'Name',
-			tpl: '<img class="station-img" src="http://rasc.ch/bryntum/scheduler-2.2.11-trial/examples/bbc/images/{Id}.png" />'
+			tpl: '<img class="station-img" src="http://rasc.ch/bryntum/scheduler-2.5.1-trial/examples/bbc/images/{Id}.png" />'
 		} ],
 		plugins: new Sch.plugin.CurrentTimeLine(),
 		viewConfig: {
